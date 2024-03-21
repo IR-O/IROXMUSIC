2075ce3bcc2fc47bcb93230011ff60b8d2e495b0
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:46 +0530

    comment

--- IroXMusic/utils/inline/start.py
+++ IroXMusic/utils/inline/start.py
@@ -6,15 +6,24 @@ import config
 from IroXMusic import app
 
 def get_button(text: str, url: str) -> InlineKeyboardButton:
+    # This function returns an InlineKeyboardButton object with the given text and url.
     return InlineKeyboardButton(text=text, url=url)
 
 def get_support_chat() -> str:
+    # This function returns the support chat URL as a string.
+    # If the SUPPORT_CHAT variable is defined in the config module, it will be used,
+    # otherwise, the default value "https://t.me/your_support_chat" will be returned.
     return config.SUPPORT_CHAT if config.SUPPORT_CHAT else "https://t.me/your_support_chat"
 
 def get_owner_id() -> int:
+    # This function returns the owner ID as an integer.
+    # If the OWNER_ID variable is defined in the config module, it will be used,
+    # otherwise, the default value 0 will be returned.
     return config.OWNER_ID if config.OWNER_ID else 0
 
 def start_panel() -> List[List[InlineKeyboardButton]]:
+    # This function returns a list of lists of InlineKeyboardButton objects,
+    # which represents the start panel.
     return [
         [
             get_button(config.START_BUTTON_1, f"https://t.me/{app.username}?startgroup=true"),
@@ -23,13 +32,12 @@ def start_panel() -> List[List[InlineKeyboardButton]]:
     ]
 
 def private_panel() -> List[List[InlineKeyboardButton]]:
+    # This function returns a list of lists of InlineKeyboardButton objects,
+    # which represents the private panel.
     support_chat = get_support_chat()
     owner_id = get_owner_id()
     return [
         [
             get_button(config.PRIVATE_BUTTON_3, f"https://t.me/{app.username}?startgroup=true"),
         ],
-        [
-            get_button(config.PRIVATE_BUTTON_4, f"settings_{owner_id}"),
-        ]
-    ]
+       
