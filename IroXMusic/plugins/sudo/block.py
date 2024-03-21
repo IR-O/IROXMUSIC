a142453daa9b22b656c004b520ca0d49794b03a4
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:45 +0530

    improve

--- IroXMusic/plugins/sudo/block.py
+++ IroXMusic/plugins/sudo/block.py
@@ -1,21 +1,26 @@
-from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram import filters, StopPropagation
+from pyrogram.types import Message, User
 
 from IroXMusic import app
-from IroXMusic.misc import SUDOERS, LOVE
+from IroXMusic.misc import SUDOERS
 from IroXMusic.utils.database import add_gban_user, remove_gban_user
 from IroXMusic.utils.decorators.language import language
 from IroXMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["block"]) & SUDOERS & LOVE)
+@app.on_message(filters.command(["block"]) & SUDOERS)
 @language
 async def useradd(client, message: Message, _):
+    if message.from_user.id in BANNED_USERS:
+        return await message.reply_text(_["block_1"].format(message.from_user.mention))
+    
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
+    if user.id == message.from_user.id:
+        return await message.reply_text(_["block_8"])
     if user.id in BANNED_USERS:
         return await message.reply_text(_["block_1"].format(user.mention))
     await add_gban_user(user.id)
@@ -23,7 +28,7 @@ async def useradd(client, message: Message, _):
     await message.reply_text(_["block_2"].format(user.mention))
 
 
-@app.on_message(filters.command(["unblock"]) & SUDOERS & LOVE)
+@app.on_message(filters.command(["unblock"]) & SUDOERS)
 @language
 async def userdel(client, message: Message, _):
     if not message.reply_to_message:
@@ -32,12 +37,16 @@ async def userdel(client, message: Message, _):
     user = await extract_user(message)
     if user.id not in BANNED_USERS:
         return await message.reply_text(_["block_3"].format(user.mention))
+    try:
+        await app.unban_chat_member(message.chat.id, user.id)
+    except:
+        return await message.reply_text(_["block_9"])
     await remove_gban_user(user.id)
     BANNED_USERS.remove(user.id)
     await message.reply_text(_["block_4"].format(user.mention))
 
 
-@app.on_message(filters.command(["blocked", "blockedusers", "blusers"]) & SUDOERS & LOVE)
+@app.on_message(filters.command(["blocked", "blockedusers", "blusers"]) & SUDOERS)
 @language
 async def sudoers_list(client, message: Message, _):
     if not BANNED_USERS:
@@ -48,12 +57,9 @@ async def sudoers_list(client, message: Message, _):
     for users in BANNED_USERS:
         try:
             user = await app.get_users(users)
+            if not user:
+                continue
             user = user.first_name if not user.mention else user.mention
             count += 1
         except:
-            continue
-        msg += f"{count}➤ {user}\n"
-    if count == 0:
-        return await mystic.edit_text(_["block_5"])
-    else:
-        return await mystic.edit_text(msg)
+
