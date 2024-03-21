e3ef3c5e94abdef7b37fcbc3dd540637a72a1b9f
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:07 +0530

    improve

--- IroXMusic/plugins/play/live.py
+++ IroXMusic/plugins/play/live.py
@@ -1,4 +1,6 @@
-from pyrogram import filters
+from pyrogram import filters, StopPropagation
+from typing import Optional
+
 from IroXMusic import YouTube, app, _
 from IroXMusic.utils.channelplay import get_channeplayCB
 from IroXMusic.utils.decorators.language import languageCB
@@ -7,20 +9,19 @@ from config import BANNED_USERS
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
 @languageCB
-async def play_live_stream(client, CallbackQuery, _):
+async def play_live_stream(client, CallbackQuery, _: dict) -> None:
+    """Play live stream from YouTube."""
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     vidid, user_id, mode, cplay, fplay = callback_request.split("|")
 
     if CallbackQuery.from_user.id != int(user_id):
-        try:
-            return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
-        except:
-            return
+        await CallbackQuery.answer(_["playcb_1"], show_alert=True)
+        raise StopPropagation
 
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
-    except:
+    except Exception:
         return
 
     video = True if mode == "v" else None
@@ -29,7 +30,7 @@ async def play_live_stream(client, CallbackQuery, _):
     try:
         await CallbackQuery.message.delete()
         await CallbackQuery.answer()
-    except:
+    except Exception:
         pass
 
     mystic = await CallbackQuery.message.reply_text(
@@ -38,24 +39,24 @@ async def play_live_stream(client, CallbackQuery, _):
 
     try:
         details, track_id = await YouTube.track(vidid, True)
-    except:
+    except Exception:
         return await mystic.edit_text(_["play_3"])
 
     ffplay = True if fplay == "f" else None
 
-    if not details["duration_min"]:
-        try:
-            await stream(
-                _,
-                mystic,
-                user_id,
-                details,
-                chat_id,
-                user_name,
-                CallbackQuery.message.chat.id,
-                video,
-                streamtype="live",
-                forceplay=ffplay,
-            )
-        except Exception as e:
-            await mystic.edit_text(str(e))
+    try:
+        async with stream(
+            _,
+            mystic,
+            user_id,
+            details,
+            chat_id,
+            user_name,
+            CallbackQuery.message.chat.id,
+            video,
+            streamtype="live",
+            forceplay=ffplay,
+        ) as stream_task:
+            await stream_task
+    except Exception as e:
+        await mystic.edit_text(str(e))
