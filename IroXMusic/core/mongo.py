c8a2e875a0cae50d2dc5f410988a5c959ca401f9
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:30 +0530

    improve

--- IroXMusic/core/mongo.py
+++ IroXMusic/core/mongo.py
@@ -1,14 +1,3 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from logging import Logger
 
-from config import MONGO_DB_URI
-
-from ..logging import LOGGER
-
-LOGGER(__name__).info("Connecting to your Mongo Database...")
-try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Iro
-    LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
-    exit()
