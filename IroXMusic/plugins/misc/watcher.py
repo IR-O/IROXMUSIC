ed8340d06c618e90ef9089d461ee9e5c7dffb226
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:38 +0530

    improve

--- IroXMusic/plugins/misc/watcher.py
+++ IroXMusic/plugins/misc/watcher.py
@@ -1,14 +1,16 @@
-from pyrogram import filters
+from pyrogram import filters, StopPropagation
 from pyrogram.types import Message
 
 from IroXMusic import app
 from IroXMusic.core.call import Irop
 
-welcome = 20
-close = 30
-
-
-@app.on_message(filters.video_chat_started, group=welcome)
-@app.on_message(filters.video_chat_ended, group=close)
-async def welcome(_, message: Message):
-    await Irop.stop_stream_force(message.chat.id)
+@app.on_message(filters.video_chat_started | filters.video_chat_ended)
+async def handle\_video\_chat(client, message: Message):
+try:
+await Irop.stop\_stream\_force(message.chat.id)
+except StopPropagation:
+pass
+if message.video\_chat\_started:
+await message.reply("Video chat started. Streaming stopped.")
+else:
+await message.reply("Video chat ended. Streaming stopped.")
