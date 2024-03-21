b3badcb592fd03ace9bf342a2adc2a6a86bfce9a
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:04 +0530

    improve

--- IroXMusic/platforms/Carbon.py
+++ IroXMusic/platforms/Carbon.py
@@ -2,14 +2,11 @@ import random
 import os
 import typing
 from pathlib import Path
-
 import aiohttp
 from aiohttp import client_exceptions
 
-
 class UnableToFetchCarbon(Exception):
+    """This class defines a custom exception named 'UnableToFetchCarbon' that inherits from the built-in 'Exception' class.
+    This exception can be raised when there is an issue with fetching carbon data."""
     pass
-    # This class defines a custom exception named 'UnableToFetchCarbon' that inherits from the built-in 'Exception' class.
-    # This exception can be raised when there is an issue with fetching carbon data.
-
 
