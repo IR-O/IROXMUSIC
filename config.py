0223f75064bab4934b2e501c69ad206b087944ab
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:31 +0530

    comment

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
+# Importing required modules
 import os
 import re
 import sys
-from typing import Optional
-
-import pyrogram
-from pyrogram.errors import Value Error
-from pydantic import BaseModel
-
-class CustomBaseModel(BaseModel):
-    def dict(self, *args, **kwargs):
-        return super().dict(exclude_unset=True, *args, **kwargs)
 
