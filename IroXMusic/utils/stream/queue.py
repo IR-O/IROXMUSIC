4a865659ce5ed92b82825adc39a669c8ba4a4ad4
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:44 +0530

    improve

--- IroXMusic/utils/stream/queue.py
+++ IroXMusic/utils/stream/queue.py
@@ -5,24 +5,30 @@ from IroXMusic.misc import db
 from IroXMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
+async def get_chat_queue(chat_id):
+    """Get the queue for a given chat id."""
+    return db.get(chat_id, [])
 
-async def put_queue(
-    chat_id,
-    original_chat_id,
-    file,
-    title,
-    duration,
-    user,
-    vidid,
-    user_id,
-    stream,
+async def add_to_queue(
+    chat_id: int,
+    original_chat_id: int,
+    file: str,
+    title: str,
+    duration: str,
+    user: str,
+    vidid: str,
+    user_id: int,
+    stream: str,
     forceplay: Union[bool, str] = None,
 ):
+    """Add a song to the queue."""
     title = title.title()
     try:
         duration_in_seconds = time_to_seconds(duration) - 3
-    except:
+    except ValueError:
         duration_in_seconds = 0
+        duration = "Invalid duration"
+
     put = {
         "title": title,
         "dur": duration,
@@ -35,40 +41,39 @@ async def put_queue(
         "seconds": duration_in_seconds,
         "played": 0,
     }
+
     if forceplay:
-        check = db.get(chat_id)
-        if check:
-            check.insert(0, put)
-        else:
-            db[chat_id] = []
-            db[chat_id].append(put)
+        queue = await get_chat_queue(chat_id)
+        queue.insert(0, put)
     else:
-        db[chat_id].append(put)
+        queue = await get_chat_queue(chat_id)
+        queue.append(put)
+
+    db[chat_id] = queue
     autoclean.append(file)
 
-
-async def put_queue_index(
-    chat_id,
-    original_chat_id,
-    file,
-    title,
-    duration,
-    user,
-    vidid,
-    stream,
+async def add_to_queue_index(
+    chat_id: int,
+    original_chat_id: int,
+    file: str,
+    title: str,
+    duration: str,
+    user: str,
+    vidid: str,
+    stream: str,
     forceplay: Union[bool, str] = None,
 ):
+    """Add a song to the queue with index checking."""
     if "20.212.146.162" in vidid:
         try:
-            dur = await asyncio.get_event_loop().run_in_executor(
-                None, check_duration, vidid
-            )
+            dur = await check_duration(vidid)
             duration = seconds_to_min(dur)
-        except:
+        except Exception:
             duration = "ᴜʀʟ sᴛʀᴇᴀᴍ"
             dur = 0
     else:
         dur = 0
+
     put = {
         "title": title,
         "dur": duration,
@@ -80,12 +85,5 @@ async def put_queue_index(
         "seconds": dur,
         "played": 0,
     }
-    if forceplay:
-        check = db.get(chat_id)
-        if check:
-            check.insert(0, put)
-        else:
-            db[chat_id] = []
-            db[chat_id].append(put)
-    else:
-        db[chat_id].append(put)
+
+    if force
