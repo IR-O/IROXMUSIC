d74f34eb52237d4ab44363011804511e90e67f01
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:15 +0530

    improve

--- IroXMusic/plugins/misc/seeker.py
+++ IroXMusic/plugins/misc/seeker.py
@@ -1,22 +1,22 @@
 import asyncio
+from IroXMusic.misc import db
+from IroXMusic.utils.database import get_active_chats, is_music_playing
 
-from IroXMusic.misc import db # Importing the db module from IroXMusic.misc for database-related functionality
-from IroXMusic.utils.database import get_active_chats, is_music_playing # Importing get_active_chats and is_music_playing functions from IroXMusic.utils.database for interacting with the database
+async def stop_music_if_playing():
+    while True:
+        # Calling get_active_chats function to get a list of active chats in the database
+        active_chats = get_active_chats()
 
-# The following lines of code could be part of a larger function or method that handles some music-related functionality in the IroXMusic bot
+        # Calling is_music_playing function to check if music is currently playing in any of the active chats
+        is_playing = is_music_playing()
 
-# Calling get_active_chats function to get a list of active chats in the database
-active_chats = get_active_chats()
+        # If music is playing in any of the active chats, the code below could be used to stop it
+        if is_playing:
+            await db.stop_music(active_chats[0])
 
-# Calling is_music_playing function to check if music is currently playing in any of the active chats
-is_playing = is_music_playing()
+        # Wait for a certain amount of time before checking the status of music playback again
+        wait_time = 10 # Wait time in seconds
+        await asyncio.sleep(wait_time)
 
-# If music is playing in any of the active chats, the code below could be used to stop it
-# For example, the following line could be used to stop the music in the first active chat where it is playing
-if is_playing:
-    db.stop_music(active_chats[0])
-
-# The following lines of code could be used to wait for a certain amount of time before checking the status of music playback again
-# This could be useful in a loop that periodically checks if music is playing and stops it if necessary
-wait_time = 10 # Wait time in seconds
-await asyncio.sleep(wait_time)
+# Call the stop_music_if_playing coroutine using asyncio.create_task
+asyncio.create_task(stop_music_if_playing())
