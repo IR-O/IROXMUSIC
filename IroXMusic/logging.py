4c192fdfdd96b7684f0f4b8d2442235a7a02b75d
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:16 +0530

    improve

--- IroXMusic/logging.py
+++ IroXMusic/logging.py
@@ -1,15 +1,50 @@
-import logging  # Import the logging module, which provides a flexible framework for emitting log messages from Python programs
+import logging.config
+import sys
 
-# Configure the logging module with some basic settings
-logging.basicConfig(
-    level=logging.INFO,  # Set the root logger level to INFO, so that all messages with level INFO and above will be tracked
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",  # Set the log format to include a timestamp, log level, name, and message
-    datefmt="%d-%b-%y %H:%M:%S",  # Set the date format to include the day, month (abbreviated), year, hour, minute, and second
-    handlers=[
-        logging.FileHandler("log.txt"),  # Create a FileHandler to write logs to a file named 'log.txt'
-        logging.StreamHandler(),  # Create a StreamHandler to write logs to the console
-    ],
-)
+# Define the log configuration
+log_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s - %(levelname)s] - %(name)s - %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'level': logging.INFO,
+            'stream': sys.stdout,
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'level': logging.INFO,
+            'filename': 'log.txt',
+            'maxBytes': 10485760,
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default', 'file'],
+            'level': logging.INFO,
+            'propagate': True,
+        },
+        'httpx': {
+            'handlers': ['file'],
+            'level': logging.ERROR,
+            'propagate': True,
+        },
+    },
+}
 
-# Set the logger level for specific modules to ERROR, so that only ERROR level messages and above will be tracked
-logging.getLogger("httpx").setLevel(logging.ERROR) 
+# Configure the logging module with the log configuration
+logging.config.dictConfig(log_config)
+
+# Get the logger
+logger = logging.getLogger()
+
+# Example log message
+logger.info('This is an info message')
