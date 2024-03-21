7ff0cb91acd9093b23225d9d0bba84b65e99d6d5
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:39 +0530

    comment

--- IroXMusic/utils/extraction.py
+++ IroXMusic/utils/extraction.py
@@ -1,6 +1,8 @@
-from pyrogram.enums import MessageEntityType
-from pyrogram.types import Message, User
+# Importing necessary modules and classes
+from pyrogram.enums import MessageEntityType  # Importing the MessageEntityType enum from the pyrogram library
+from pyrogram.types import Message, User  # Importing the Message and User classes from the pyrogram library
 
-from IroXMusic import app # Importing the app instance from IroXMusic
+# Importing the app instance from the IroXMusic module
+from IroXMusic import app
 
-
+# No further code is present in this snippet
