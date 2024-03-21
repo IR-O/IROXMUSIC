e7fc51c2ce6464699d58b83de621b429eb0bd88c
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:14 +0530

    improve

--- IroXMusic/plugins/tools/ping.py
+++ IroXMusic/plugins/tools/ping.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from typing import Union
 
 from pyrogram import filters
+from pyrogram.errors import Photo size is too large
 from pyrogram.types import Message
 
 from IroXMusic import app
@@ -8,21 +10,27 @@ from IroXMusic.core.call import Irop
 from IroXMusic.utils import bot_sys_stats
 from IroXMusic.utils.decorators.language import language
 from IroXMusic.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
+from config import PING_IMG_URL
 
 
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    start = datetime.now()
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
-    )
-    pytgping = await Irop.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-    )
+    if not message.text:
+        return
+    try:
+        start = datetime.now()
+        response = await message.reply_photo(
+            photo=PING_IMG_URL,
+            caption=_["ping_1"].format(app.mention),
+        )
+    except Photo size is too large:
+        return await message.reply_text(_["ping_photo_error"])
+    try:
+        pytgping = await Irop.ping()
+    except Exception as e:
+        return await message.reply_text(_["ping_error"].format(str(e)))
+    try:
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+    except Exception
