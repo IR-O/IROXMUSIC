b16b491a8d372bf438bb9e87a1e4e0ad8587e6fd
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:33 +0530

    improve

--- IroXMusic/utils/inline/speed.py
+++ IroXMusic/utils/inline/speed.py
@@ -1,41 +1,46 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+def speed_markup(chat_id, _):
+    """
+    Generates an InlineKeyboardMarkup object for speed controls.
 
-def speed_markup(_, chat_id):
-    upl = InlineKeyboardMarkup(
+    :param chat_id: The ID of the chat.
+    :param _: The translation dictionary.
+    :return: An InlineKeyboardMarkup object.
+    """
+    button_data_template = "SpeedUP {}"
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text="🕒 0.5x",
-                    callback_data=f"SpeedUP {chat_id}|0.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕓 0.75x",
-                    callback_data=f"SpeedUP {chat_id}|0.75",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["P_B_4"],
-                    callback_data=f"SpeedUP {chat_id}|1.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🕤 1.5x",
-                    callback_data=f"SpeedUP {chat_id}|1.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕛 2.0x",
-                    callback_data=f"SpeedUP {chat_id}|2.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ],
-        ]
-    )
-    return upl
+            InlineKeyboardButton(
+                text="🕒 0.5x",
+                callback_data=button_data_template.format(button_data_template.format(chat_id, "0.5")),
+            ),
+            InlineKeyboardButton(
+                text="🕓 0.75x",
+                callback_data=button_data_template.format(button_data_template.format(chat_id, "0.75")),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["P_B_4"],
+                callback_data=button_data_template.format(button_data_template.format(chat_id, "1.0")),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🕤 1.5x",
+                callback_data=button_data_template.format(button_data_template.format(chat_id, "1.5")),
+            ),
+            InlineKeyboardButton(
+                text="🕛 2.0x",
+                callback_data=button_data_template.format(button_data_template.format(chat_id, "2.0")),
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+            ),
+        ],
+    ]
+    return InlineKeyboardMarkup(buttons)
