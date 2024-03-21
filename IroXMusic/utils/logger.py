bf1c1a8e28f02e563a42902a345422d9edfc25e8
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:45 +0530

    comment

--- IroXMusic/utils/logger.py
+++ IroXMusic/utils/logger.py
@@ -1,41 +1,22 @@
-from pyrogram.enums import ParseMode
-from pyrogram.errors import ChatAdminRequired, UserIsBlocked
+from pyrogram.enums import ParseMode  # Importing ParseMode enum from pyrogram library
+from pyrogram.errors import ChatAdminRequired, UserIsBlocked  # Importing required exceptions
 
-from IroXMusic import app, LOGGER_ID, USERNAME
-from IroXMusic.utils.database import is_on_off
-from IroXMusic.utils.decorators import errors
+from IroXMusic import app, LOGGER_ID, USERNAME  # Importing app, LOGGER_ID, and USERNAME from the IroXMusic module
+from IroXMusic.utils.database import is_on_off  # Importing is_on_off function from the database utilities
+from IroXMusic.utils.decorators import errors  # Importing errors decorator
 
+@errors  # Decorating the function with errors decorator
+async def play_logs(message, streamtype):  # Defining the asynchronous function play_logs with message and streamtype as parameters
+    if not await is_on_off(2):  # If the value returned by is_on_off(2) is False
+        return  # Exit the function
 
-@errors
-async def play\_logs(message, streamtype):
-if not await is\_on\_off(2):
-return
+    try:
+        logger_text = f"""  # Initializing the logger_text variable with the following formatted string
+            <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>  # Bold text for 'IroXMusic Play Log'
 
-try:
-logger\_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+            <b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>  # Bold text for 'Chat ID' followed by the chat ID in code format
+            <b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}  # Bold text for 'Chat Name' followed by the chat title
+            <b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}  # Bold text for 'Chat Username' followed by the chat username
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
-
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from\_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from\_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from\_user.username}
-
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-if message.chat.id != LOGGER\_ID:
-await app.send\_message(
-chat\_id=LOGGER\_ID,
-text=logger\_text,
-parse\_mode=ParseMode.HTML,
-disable\_web\_page\_preview=True,
-)
-except ChatAdminRequired:
-await message.reply\_text(
-"⚠️ **I'm not a admin in this chat, please add me as an admin to send logs.**",
-parse\_mode=ParseMode.MARKDOWN
-)
-except UserIsBlocked:
-await message.
+            <b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>  # Bold text for 'User ID' followed by the user ID in code format
+            <b>ɴᴀᴍᴇ :
