445a3d3a9355c5df5a4ac98a458227efa0f250f6
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:47:33 +0530

    comment

--- IroXMusic/utils/inline/settings.py
+++ IroXMusic/utils/inline/settings.py
@@ -2,8 +2,17 @@ from typing import List, Union
 
 from pyrogram.types import InlineKeyboardButton, CallbackQuery
 
-
 def setting_markup(locale_dict: dict) -> List[List[InlineKeyboardButton]]:
+    """
+    This function returns a list of lists of InlineKeyboardButton objects,
+    which can be used to create a settings menu in a Pyrogram bot.
+
+    Args:
+    locale_dict (dict): A dictionary containing localized text for the buttons.
+
+    Returns:
+    List[List[InlineKeyboardButton]]: A list of lists of InlineKeyboardButton objects.
+    """
     buttons = [
         [
             InlineKeyboardButton(text=locale_dict["ST_B_1"], callback_data="AU"),
@@ -21,10 +30,21 @@ def setting_markup(locale_dict: dict) -> List[List[InlineKeyboardButton]]:
     ]
     return buttons
 
-
 def vote_mode_markup(
     locale_dict: dict, current_value: str, mode: Union[bool, str] = None
 ) -> List[List[InlineKeyboardButton]]:
+    """
+    This function returns a list of lists of InlineKeyboardButton objects,
+    which can be used to create a vote mode menu in a Pyrogram bot.
+
+    Args:
+    locale_dict (dict): A dictionary containing localized text for the buttons.
+    current_value (str): The current value of the vote mode.
+    mode (Union[bool, str], optional): The mode of the vote mode. Defaults to None.
+
+    Returns:
+    List[List[InlineKeyboardButton]]: A list of lists of InlineKeyboardButton objects.
+    """
     buttons = [
         [
             InlineKeyboardButton(text="Vᴏᴛɪɴɢ ᴍᴏᴅᴇ ➜", callback_data="VOTEANSWER"),
@@ -51,8 +71,18 @@ def vote_mode_markup(
     ]
     return buttons
 
-
 def auth_users_markup(locale_dict: dict, status: Union[bool, str] = None) -> List[List[InlineKeyboardButton]]:
+    """
+    This function returns a list of lists of InlineKeyboardButton objects,
+    which can be used to create an authorization users menu in a Pyrogram bot.
+
+    Args:
+    locale_dict (dict): A dictionary containing localized text for the buttons.
+    status (Union[bool, str], optional): The status of the authorization users. Defaults to None.
+
+    Returns:
+    List[List[InlineKeyboardButton]]: A list of lists of InlineKeyboardButton objects.
+    """
     buttons = [
         [
             InlineKeyboardButton(text=locale_dict["ST_B_7"], callback_data="AUTHANSWER"),
@@ -74,13 +104,25 @@ def auth_users_markup(locale_dict: dict, status: Union[bool, str] = None) -> Lis
     ]
     return buttons
 
-
 def playmode_users_markup(
     locale_dict: dict,
     direct: Union[bool, str] = None,
     group: Union[bool, str] = None,
     playtype: Union[bool, str] = None,
 ) -> List[List[InlineKeyboardButton]]:
+    """
+    This function returns a list of lists of InlineKeyboardButton objects,
+    which can be used to create a play mode users menu in a Pyrogram bot.
+
+    Args:
+    locale_dict (dict): A dictionary containing localized text for the buttons.
+    direct (Union[bool, str], optional): The direct status of the play mode users. Defaults to None.
+    group (Union[bool, str], optional): The group status of the play mode users. Defaults to None.
+    playtype (Union[bool, str], optional): The playtype status of the play mode users. Defaults to None.
+
+    Returns:
+    List[List[InlineKeyboardButton]]: A list of lists of InlineKeyboardButton objects.
+    """
     buttons = [
         [
             InlineKeyboardButton(text=locale_dict["ST_B_10"], callback_data="SEARCHANSWER"),
@@ -92,23 +134,4 @@ def playmode_users_markup(
         [
             InlineKeyboardButton(text=locale_dict["ST_B_13"], callback_data="AUTHANSWER"),
             InlineKeyboardButton(
-                text=locale_dict["ST_B_8"] if group else locale_dict["ST_B_9"],
-                callback_data="CHANNELMODECHANGE",
-            ),
-        ],
-        [
-            InlineKeyboardButton(text=locale_dict["ST_B_14"], callback_data="PLAYTYPEANSWER"),
-            InlineKeyboardButton(
-                text=locale_dict["ST_B_8"] if playtype else locale_dict["ST_B_9"],
-                callback_data="PLAYTYPECHANGE",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text=locale_dict["BACK_BUTTON"],
-                callback_data="settings_helper",
-            ),
-            InlineKeyboardButton(text=locale_dict["CLOSE_BUTTON"], callback_data="close"),
-        ],
-    ]
-    return buttons
+                text=locale_dict["ST
