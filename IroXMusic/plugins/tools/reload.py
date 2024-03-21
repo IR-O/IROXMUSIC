51896589c7ab55998785a62f47cc0fdd3e9636d7
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:45 +0530

    comment

--- IroXMusic/plugins/tools/reload.py
+++ IroXMusic/plugins/tools/reload.py
@@ -1,16 +1,20 @@
 import asyncio
 import time
 
+# Pyrogram libraries
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from IroXMusic import app
+# IroXMusic modules
+import app
 from IroXMusic.core.call import Irop
 from IroXMusic.misc import db
 from IroXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from IroXMusic.utils.decorators import ActualAdminCB, AdminActual, language
+from IroXMusic.utils.decorators import ActualAdminCB, AdminActual
 from IroXMusic.utils.formatters import alpha_to_int, get_readable_time
+
+# Configuration
 from config import BANNED_USERS, adminlist, lyrical
 
 
