bf6c93056af31581328a2587d07dc36c8f3f51a7
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:23 +0530

    improve

--- IroXMusic/utils/decorators/__init__.py
+++ IroXMusic/utils/decorators/__init__.py
@@ -1,22 +1,25 @@
-# admins.py and language.py should be in the same directory as this file
-
-# admins.py
 from telegram import Bot, User
 
-admin_list = []
+_admin_list = []
 
 def add_admin(user_id):
     user = User(user_id)
-    admin_list.append(user)
+    _admin_list.append(user)
 
 def is_admin(user_id):
     user = User(user_id)
-    return user in admin_list
+    return user in _admin_list
 
-__all__ = ['add_admin', 'is_admin']
+# Expose the functions using __getattr__ to prevent them from being imported as variables
+def __getattr__(name):
+    if name == "add_admin":
+        return add_admin
+    elif name == "is_admin":
+        return is_admin
+    else:
+        raise AttributeError
 
 
-# language.py
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def create_language_keyboard():
@@ -32,14 +35,19 @@ def create_language_keyboard():
     ]
     return InlineKeyboardMarkup(keyboard)
 
-__all__ = ['create_language_keyboard']
+# Expose the function using __getattr__ to prevent it from being imported as a variable
+def __getattr__():
+    return create_language_keyboard
 
 
-# main.py
 from telegram import Update
-from .admins import *
-from .language import *
+import admins
+import language
 
 def some_function(update: Update):
     # use add_admin and is_admin functions from admins.py
+    admins.add_admin(update.effective_user.id)
+    is_admin = admins.is_admin(update.effective_user.id)
+
     # use create_language_keyboard function from language.py
+    keyboard = language.create_language_keyboard()
