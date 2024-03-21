dbe40fa9bf243fe4331f256799157fda27eed6ba
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:17 +0530

    improve

--- IroXMusic/plugins/play/playmode.py
+++ IroXMusic/plugins/play/playmode.py
@@ -8,39 +8,26 @@ from IroXMusic.utils.decorators import language
 from IroXMusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
-
 @app.on_message(filters.command(["playmode", "mode"]) & filters.group & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
-    try:
-        playmode = await get_playmode(message.chat.id)
-    except Exception as e:
-        await message.reply_text(_["error_1"].format(str(e)))
-        return
-
-    direct: Optional[bool] = None
-    if playmode == "Direct":
-        direct = True
-
     try:
         is_non_admin = await is_nonadmin_chat(message.chat.id)
     except Exception as e:
         await message.reply_text(_["error_1"].format(str(e)))
         return
 
-    group: Optional[bool] = None
-    if not is_non_admin:
-        group = True
-
     try:
+        playmode = await get_playmode(message.chat.id)
         playty = await get_playtype(message.chat.id)
+
+        direct = playmode == "Direct" if is_non_admin else None
+        group = not is_non_admin if playmode == "Group" else None
+        playtype = playty == "Everyone" if group else playty
+
     except Exception as e:
         await message.reply_text(_["error_1"].format(str(e)))
         return
 
-    playtype: Optional[bool] = None
-    if playty == "Everyone":
-        playtype = None
-    else:
-        playtype = True
-
+    # Do something with direct, group, and playtype
+    # ...
