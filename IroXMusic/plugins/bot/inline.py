ea9bae7512e250e26817e0551e1dbd5a430377a4
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:47:08 +0530

    comment

--- IroXMusic/plugins/bot/inline.py
+++ IroXMusic/plugins/bot/inline.py
@@ -3,35 +3,40 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineQueryResultPhoto,
 )
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch  # Importing VideosSearch from youtubesearchpython package
 
-from IroXMusic import app
-from IroXMusic.utils.inlinequery import answer
-from config import BANNED_USERS
+from IroXMusic import app  # Importing app instance from IroXMusic package
+from IroXMusic.utils.inlinequery import answer  # Importing answer function from inlinequery.py
+from config import BANNED_USERS  # Importing BANNED_USERS from config.py
 
+@app.on_inline_query(~BANNED_USERS)  # Decorator to handle inline queries from users not in BANNED_USERS list
+async def inline_query_handler(client, query):  # Defining the inline query handler function
+    text = query.query.strip().lower()  # Stripping and converting query text to lowercase
+    answers = []  # Initializing an empty list to store the answers
 
-@app.on_inline_query(~BANNED_USERS)
-async def inline_query_handler(client, query):
-    text = query.query.strip().lower()
-    answers = []
-    if text.strip() == "":
+    if text.strip() == "":  # If the query is empty
         try:
-            await client.answer_inline_query(query.id, results=answer, cache_time=10)
+            await client.answer_inline_query(query.id, results=answer, cache_time=10)  # Answer the inline query with the cached answer
         except:
-            return
+            return  # If any exception occurs, return from the function
     else:
-        a = VideosSearch(text, limit=20)
-        result = (await a.next()).get("result")
-        for x in range(15):
-            title = (result[x]["title"]).title()
-            duration = result[x]["duration"]
-            views = result[x]["viewCount"]["short"]
-            thumbnail = result[x]["thumbnails"][0]["url"].split("?")[0]
-            channellink = result[x]["channel"]["link"]
-            channel = result[x]["channel"]["name"]
-            link = result[x]["link"]
-            published = result[x]["publishedTime"]
+        a = VideosSearch(text, limit=20)  # Search for videos using VideosSearch with the query text and limit of 20
+        result = (await a.next()).get("result")  # Get the search result
+
+        for x in range(15):  # Loop through the first 15 results
+            title = (result[x]["title"]).title()  # Get the title of the video and convert it to title case
+            duration = result[x]["duration"]  # Get the duration of the video
+            views = result[x]["viewCount"]["short"]  # Get the short view count of the video
+            thumbnail = result[x]["thumbnails"][0]["url"].split("?")[0]  # Get the thumbnail URL of the video
+            channellink = result[x]["channel"]["link"]  # Get the channel link of the video
+            channel = result[x]["channel"]["name"]  # Get the channel name of the video
+            link = result[x]["link"]  # Get the video link
+            published = result[x]["publishedTime"]  # Get the published time of the video
+
+            # Construct the description string
             description = f"{views} | {duration} ᴍɪɴᴜᴛᴇs | {channel}  | {published}"
+
+            # Construct the InlineKeyboardMarkup for the InlineQueryResultPhoto
             buttons = InlineKeyboardMarkup(
                 [
                     [
@@ -42,6 +47,8 @@ async def inline_query_handler(client, query):
                     ],
                 ]
             )
+
+            # Construct the searched_text string
             searched_text = f"""
 ❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
 
@@ -51,18 +58,4 @@ async def inline_query_handler(client, query):
 ⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
 
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
-            answers.append(
-                InlineQueryResultPhoto(
-                    photo_url=thumbnail,
-                    title=title,
-                    thumb_url=thumbnail,
-                    description=description,
-                    caption=searched_text,
-                    reply_markup=buttons,
-                )
-            )
-        try:
-            return await client.answer_inline_query(query.id, results=answers)
-        except:
-            return
+<u>
