1ce3bf3d056cdbefafe13088017473293234fbca
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:01 +0530

    improve

--- IroXMusic/plugins/tools/reload.py
+++ IroXMusic/plugins/tools/reload.py
@@ -2,11 +2,11 @@ import asyncio
 import time
 
 # Pyrogram libraries
-from pyrogram import filters
+import pyrogram
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-# IroXMusic modules
+# IroXMusic libraries
 import app
 from IroXMusic.core.call import Irop
 from IroXMusic.misc import db
@@ -15,6 +15,10 @@ from IroXMusic.utils.decorators import ActualAdminCB, AdminActual
 from IroXMusic.utils.formatters import alpha_to_int, get_readable_time
 
 # Configuration
-from config import BANNED_USERS, adminlist, lyrical
+import config
 
-
+# Assigning the imported modules to variables with lowercase names is a common convention
+# to distinguish between classes and functions (uppercase) and variables (lowercase)
+banned_users = config.BANNED_USERS
+adminlist = config.adminlist
+lyrical = config.lyrical
