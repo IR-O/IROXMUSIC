54f4e277141716bde3fc26dc96ba0110941998f5
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:33 +0530

    improve

--- IroXMusic/utils/formatters.py
+++ IroXMusic/utils/formatters.py
@@ -1,22 +1,17 @@
 import json
 import subprocess
 
-
 def get_readable_time(seconds: int) -> str:
     """
     Convert a given number of seconds into a human-readable time string in the format of "Xh Ym Zs".
-
-    This function calculates the time components (hours, minutes, and seconds) by repeatedly dividing the input
-    seconds value by 60 until all the time components are calculated. The time components are then formatted
-    as a string with their respective time suffixes.
 
     :param seconds: The number of seconds to convert to a human-readable time string.
     :return: A string representation of the time in the format of "Xh Ym Zs".
     """
     time_list = []
-    time_suffix_list = ["s", "m", "h", "d"]
+    time_suffix_list = ["s", "m", "h"]
 
-    for i in range(4):
+    for i in range(3):
         if seconds == 0:
             break
         div, mod = divmod(seconds, 60)
@@ -25,19 +20,14 @@ def get_readable_time(seconds: int) -> str:
 
     time_list = [f"{t}{suf}" for t, suf in zip(reversed(time_list), time_suffix_list)]
 
-    if len(time_list) == 4:
+    if len(time_list) == 3:
         time_list[-1], time_list[-2] = time_list[-2], time_list[-1]
 
     return ":".join(time_list)
 
-
 def convert_bytes(size: float) -> str:
     """
     Convert a given number of bytes into a human-readable string with appropriate units.
-
-    This function converts the input size in bytes to the appropriate unit (KiB, MiB, GiB, or TiB) by repeatedly
-    dividing the input size by 1024 until the resulting size is less than 1024. The final size is then formatted
-    as a string with two decimal places and the corresponding unit.
 
     :param size: The number of bytes to convert to a human-readable string.
     :return: A string representation of the size with appropriate units.
@@ -46,28 +36,23 @@ def convert_bytes(size: float) -> str:
         return ""
     power = 1024
     t_n = 0
-    power_dict = {0: " ", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    power_dict = {0: "B", 1: "KiB", 2: "MiB", 3: "GiB", 4: "TiB"}
 
     while size > power:
         size /= power
         t_n += 1
 
-    return f"{size:.2f} {power_dict[t_n]}B"
-
+    return f"{size:.2f} {power_dict[t_n]}"
 
 def convert_base(n: str, to_base: int = 10) -> str:
     """
     Convert a number from base 10 to a given base.
 
-    This function converts a base-10 number to a given base by repeatedly dividing the input number by the base
-    until the quotient is zero. The remainders from the divisions are then used to build the number in the
-    desired base. The digits in the resulting number are represented using an alphabet of lowercase letters.
-
     :param n: The base-10 number to convert.
     :param to_base: The base to convert the number to. Default is 10.
     :return: A string representation of the number in the desired base.
     """
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    alphabet = "0123456789abcdefghij"
     result = ""
 
     for char in n[::-1]:
@@ -82,13 +67,9 @@ def convert_base(n: str, to_base: int = 10) -> str:
 
     return result[::-1]
 
-
 def time_to_seconds(time: str) -> int:
     """
     Convert a time string in the format of "Xh Ym Zs" to seconds.
-
-    This function parses a time string in the format of "Xh Ym Zs" and converts it to seconds by calculating
-    the number of hours, minutes, and seconds and multiplying each by the appropriate number of seconds.
 
     :param time: The time string to convert to seconds.
     :return: The number of seconds represented by the time string.
@@ -96,13 +77,9 @@ def time_to_seconds(time: str) -> int:
     h, m, s = map(int, time.split(":"))
     return h * 3600 + m * 60 + s
 
-
 def seconds_to_min(seconds: int) -> str:
     """
     Convert a given number of seconds to a formatted time string.
-
-    This function converts a given number of seconds to a formatted time string in the format of "Xh Ym Zs".
-    If the input seconds value is less than 60, the function returns a string in the format of "Xm Zs".
 
     :param seconds: The number of seconds to convert to a formatted time string.
     :return: A string representation of the time in the format of "Xh Ym Zs" or "Xm Zs".
@@ -110,4 +87,4 @@ def seconds_to_min(seconds: int) -> str:
     if seconds is None:
         return ""
     d, h, m, s = divmod(seconds, 86400), divmod(seconds, 3600), divmod(seconds, 60), seconds % 60
-    return f"{d:02d}:{h:02d}:{m:02d}:{s:02d}" if d else f"{h:02d}:{m
+    return f"{h:02d}:{m:02d}:{s:02d}" if d else f"{m:02d}:{s:02d}"
