616f26bb303c3d8f3fb3ea062e4b489d601a5ea1
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:35 +0530

    comment

--- IroXMusic/utils/inlinequery.py
+++ IroXMusic/utils/inlinequery.py
@@ -1,8 +1,10 @@
-# Define a list of actions and corresponding command strings
-actions = [
-    # Action name: Pause
-    ("PᴀᴜSᴇ", "/pause"),
+# Define a list of custom actions with their corresponding command strings
+# Each action is a tuple containing the action name (in title case) and its corresponding command string
 
-    # Action name: Resume
-    ("Rᴇsᴜᴍᴇ", "/resume"),
+# For example, the first action is "Pause" with the command string "/pause"
+actions = [
+    ("PᴀᴜSᴇ", "/pause"),  # Action name: Pause, Command string: /pause
+
+    # The second action is "Resume" with the command string "/resume"
+    ("Rᴇsᴜᴍᴇ", "/resume"),  # Action name: Resume, Command string: /resume
 ]
