c673ef602321aa9260bb60938ce9f96e7cd4831a
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:52:57 +0530

    improve

--- IroXMusic/plugins/misc/seeker.py
+++ IroXMusic/plugins/misc/seeker.py
@@ -5,19 +5,13 @@ from IroXMusic.utils.database import get_active_chats, is_music_playing # Import
 async def stop_music_if_playing():
     """Coroutine to continuously check if music is playing in any active chats and stop it if necessary."""
     while True:
-        # Get a list of active chats in the database
-        active_chats = get_active_chats()
+        try:
+            # Get a list of active chats in the database
+            active_chats = await get_active_chats()
 
-        # Check if music is currently playing in any of the active chats
-        is_playing = is_music_playing()
+            # Check if music is currently playing in any of the active chats
+            is_playing = await is_music_playing()
 
-        # If music is playing in any of the active chats, stop it
-        if is_playing:
-            await db.stop_music(active_chats[0])
-
-        # Wait for a certain amount of time before checking the status of music playback again
-        wait_time = 10 # Wait time in seconds
-        await asyncio.sleep(wait_time)
-
-# Call the stop_music_if_playing coroutine using asyncio.create_task
-asyncio.create_task(stop_music_if_playing())
+            # If music is playing in any of the active chats, stop it
+            if is_playing:
+              
