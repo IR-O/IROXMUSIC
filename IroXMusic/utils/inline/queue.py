c4445233be391f3f51f37ed14b4d3bc37234502f
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:47:04 +0530

    comment

--- IroXMusic/utils/inline/queue.py
+++ IroXMusic/utils/inline/queue.py
@@ -1,21 +1,21 @@
-from typing import Union
+from typing import Union  # Importing the Union type from typing module
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup  # Importing necessary classes from pyrogram.types module
 
-
+# Function to create queue markup
 def queue_markup(
-    _,
-    DURATION,
-    CPLAY,
-    videoid,
-    played: Union[bool, int] = None,
-    dur: Union[bool, int] = None,
+    _,  # Underscore is used as a conventional name for the first parameter in decorators
+    DURATION,  # The duration of the video
+    CPLAY,  # The command play
+    videoid,  # The ID of the video
+    played: Union[bool, int] = None,  # Whether the video has been played or not, or the elapsed time of the video
+    dur: Union[bool, int] = None,  # The duration of the video or a boolean value indicating if the duration is known
 ):
-    not_dur = [
+    not_dur = [ # If the duration is unknown, create a list of InlineKeyboardButton objects
         [
             InlineKeyboardButton(
-                text=_["QU_B_1"],
-                callback_data=f"GetQueued {CPLAY}|{videoid}",
+                text=_["QU_B_1"],  # The text on the button
+                callback_data=f"GetQueued {CPLAY}|{videoid}",  # The data to be sent along with the callback query
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -23,17 +23,17 @@ def queue_markup(
             ),
         ]
     ]
-    dur = [
+    dur = [ # If the duration is known, create a list of InlineKeyboardButton objects
         [
             InlineKeyboardButton(
-                text=_["QU_B_2"].format(played, dur),
-                callback_data="GetTimer",
+                text=_["QU_B_2"].format(played, dur),  # The text on the button
+                callback_data="GetTimer",  # The data to be sent along with the callback query
             )
         ],
         [
             InlineKeyboardButton(
-                text=_["QU_B_1"],
-                callback_data=f"GetQueued {CPLAY}|{videoid}",
+                text=_["QU_B_1"],  # The text on the button
+                callback_data=f"GetQueued {CPLAY}|{videoid}",  # The data to be sent along with the callback query
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -41,35 +41,31 @@ def queue_markup(
             ),
         ],
     ]
-    upl = InlineKeyboardMarkup(not_dur if DURATION == "Unknown" else dur)
-    return upl
+    upl = InlineKeyboardMarkup(not_dur if DURATION == "Unknown" else dur)  # Create an InlineKeyboardMarkup object with the appropriate list of buttons
+    return upl  # Return the InlineKeyboardMarkup object
 
 
+# Function to create queue back markup
 def queue_back_markup(_, CPLAY):
     upl = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"queue_back_timer {CPLAY}",
+                    text=_["BACK_BUTTON"],  # The text on the button
+                    callback_data=f"queue_back_timer {CPLAY}",  # The data to be sent along with the callback query
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
-                ),
+              `enter code here`                ),
             ]
         ]
     )
-    return upl
+    return upl  # Return the InlineKeyboardMarkup object
 
 
+# Function to create admin markup
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-    ]
-    return buttons
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),  # The text on the button and
