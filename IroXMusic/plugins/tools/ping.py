7bd9afb9ace7d72ed97adfc71f69019d7094b6c8
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:45 +0530

    comment

--- IroXMusic/plugins/tools/ping.py
+++ IroXMusic/plugins/tools/ping.py
@@ -1,36 +1,26 @@
-from datetime import datetime
-from typing import Union
+from datetime import datetime # Importing datetime module to get current time
+from typing import Union # Importing Union from typing module
 
-from pyrogram import filters
-from pyrogram.errors import Photo size is too large
-from pyrogram.types import Message
+from pyrogram import filters # Importing filters from pyrogram module
+from pyrogram.errors import Photo size is too large # Importing specific error from pyrogram module
+from pyrogram.types import Message # Importing Message from pyrogram module
 
-from IroXMusic import app
-from IroXMusic.core.call import Irop
-from IroXMusic.utils import bot_sys_stats
-from IroXMusic.utils.decorators.language import language
-from IroXMusic.utils.inline import supp_markup
-from config import BANNED_USERS
-from config import PING_IMG_URL
+from IroXMusic import app # Importing app from IroXMusic module
+from IroXMusic.core.call import Irop # Importing Irop from IroXMusic.core.call module
+from IroXMusic.utils import bot_sys_stats # Importing bot_sys_stats from IroXMusic.utils module
+from IroXMusic.utils.decorators.language import language # Importing language from IroXMusic.utils.decorators.language module
+from IroXMusic.utils.inline import supp_markup # Importing supp_markup from IroXMusic.utils.inline module
+from config import BANNED_USERS # Importing BANNED_USERS from config module
+from config import PING_IMG_URL # Importing PING_IMG_URL from config module
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
-@language
-async def ping_com(client, message: Message, _):
-    if not message.text:
-        return
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS) # Decorator to listen for /ping and /alive commands from users not in BANNED_USERS list
+@language # Decorator to handle language-specific translations
+async def ping_com(client, message: Message, _): # Defining the asynchronous function ping_com with client, message and _ as parameters
+    if not message.text: # Checking if message text is empty
+        return # Returning if message text is empty
     try:
-        start = datetime.now()
-        response = await message.reply_photo(
-            photo=PING_IMG_URL,
-            caption=_["ping_1"].format(app.mention),
-        )
-    except Photo size is too large:
-        return await message.reply_text(_["ping_photo_error"])
-    try:
-        pytgping = await Irop.ping()
-    except Exception as e:
-        return await message.reply_text(_["ping_error"].format(str(e)))
-    try:
-        UP, CPU, RAM, DISK = await bot_sys_stats()
-    except Exception
+        start = datetime.now() # Getting current time before sending photo
+        response = await message.reply_photo( # Replying to the message with a photo
+            photo=PING_IMG_URL, # Using PING_IMG_URL as the photo
+            caption=_["ping_1"].format(app.mention), # Adding caption to the photo using
