1bd008702b39011323ab68b42b3c8983f3d13f7d
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:10 +0530

    improve

--- IroXMusic/plugins/tools/active.py
+++ IroXMusic/plugins/tools/active.py
@@ -1,8 +1,10 @@
-from pyrogram import filters
-from pyrogram.types import Message
-from unidecode import unidecode
+from typing import List, AsyncIterator
 
+from pyrogram import filters, Message
+from pyrogram.types import Chat
 from pyrogram.errors import ChatNotFound
+
+from unidecode import unidecode
 
 from IroXMusic import app
 from IroXMusic.misc import SUDOERS, LOVE
@@ -18,7 +20,7 @@ from IroXMusic.utils.database import (
 async def active_chats(_, message: Message):
     mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
     chat_type = message.command[1]
-    served_chats = []
+    served_chats: List[str] = []
     if chat_type == "vc" or chat_type == "voice":
         served_chats = await get_active_chats()
     elif chat_type == "v" or chat_type == "video":
@@ -30,23 +32,12 @@ async def active_chats(_, message: Message):
         return await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ {chat_type.upper()} ᴄʜᴀᴛs ᴏɴ {app.mention}.")
 
     text = f"» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ {chat_type.upper()} ᴄʜᴀᴛs :\n\n"
-    for j, x in enumerate(served_chats, start=1):
+    async for x in served_chats:
         try:
-            chat = await app.get_chat(x)
+            chat: Chat = await app.get_chat(x)
             if chat.type.value != "private":
                 title = chat.title
             else:
                 title = "Private Chat"
         except ChatNotFound:
-            await remove_active_chat(x) if chat_type == "vc" or chat_type == "voice" else remove_active_video_chat(x)
-            continue
-        try:
-            if chat.username:
-                user = chat.username
-                text += f"<b>{j}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
-            else:
-                text += f"<b>{j}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
-        except:
-            continue
-
-    await mystic.edit_text(text, disable_web_page_preview=True)
+            await remove_active_chat(x) if chat_type == "vc" or chat_type == "voice" else remove_active_video_chat(x
