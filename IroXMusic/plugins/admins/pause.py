8d6a60b96b1e06e23f28c74eb34cf3f58c95fa66
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:47:04 +0530

    comment

--- IroXMusic/plugins/admins/pause.py
+++ IroXMusic/plugins/admins/pause.py
@@ -1,30 +1,20 @@
-from pyrogram import filters
-from pyrogram.errors import UserNotEmpty, UserIsBot
-from pyrogram.types import Message, User
-from typing import Optional
+from pyrogram import filters  # Importing filters from pyrogram library
+from pyrogram.errors import UserNotEmpty, UserIsBot  # Importing error classes from pyrogram library
+from pyrogram.types import Message, User  # Importing Message and User classes from pyrogram library
+from typing import Optional  # Importing Optional type from typing library
 
-from IroXMusic import app
-from IroXMusic.core.call import Irop
-from IroXMusic.utils.database import is_music_playing, music_off
-from IroXMusic.utils.decorators import AdminRightsCheck
-from IroXMusic.utils.inline import close_markup
-from config import BANNED_USERS
-from language import _
+from IroXMusic import app  # Importing app instance from IroXMusic package
+from IroXMusic.core.call import Irop  # Importing Irop class from IroXMusic.core.call module
+from IroXMusic.utils.database import is_music_playing, music_off  # Importing is_music_playing and music_off functions from IroXMusic.utils.database module
+from IroXMusic.utils.decorators import AdminRightsCheck  # Importing AdminRightsCheck decorator from IroXMusic.utils.decorators module
+from IroXMusic.utils.inline import close_markup  # Importing close_markup function from IroXMusic.utils.inline module
+from config import BANNED_USERS  # Importing BANNED_USERS from config module
+from language import _  # Importing translation function from language module
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
-@AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id: int):
-    if not await is_music_playing(chat_id):
-        if not await message.chat.get_members(message.from_user.id):
-            return await message.reply_text(_["not_admin"])
-        return await message.reply_text(_["admin_1"])
-    try:
-        await music_off(chat_id)
-        await Irop.pause_stream(chat_id)
-        await message.reply_text(
-            _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
-        )
-    except UserNotEmpty:
-        await message.reply_text(_["admin_3"])
-    except UserIsBot:
-        await message.reply_text(_["admin_4"])
+@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)  # Decorator to listen for /pause or /cpause command in group chats excluding banned users
+@AdminRightsCheck  # Decorator to check for admin rights
+async def pause_admin(cli, message: Message, _, chat_id: int):  # Function to pause the music
+    if not await is_music_playing(chat_id):  # Check if music is already playing
+        if not await message.chat.get_members(message.from_user.id):  # Check if the user is a member of the chat
+            return await message.reply_text(_["not_admin"])  # Reply with not_admin message if the user is not a member
+        return await message.reply_text(_["admin_1"])  # Rep
