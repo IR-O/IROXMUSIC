7c303a18afc9c214d67c62469c77976ac06254cf
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:33 +0530

    comment

--- IroXMusic/platforms/Carbon.py
+++ IroXMusic/platforms/Carbon.py
@@ -1,12 +1,15 @@
+# Import necessary modules
 import random
 import os
 import typing
 from pathlib import Path
-import aiohttp
-from aiohttp import client_exceptions
+import aiohttp  # Asynchronous HTTP client/server for Python
+from aiohttp import client_exceptions  # Exceptions for aiohttp client
 
+# Define a custom exception class named 'UnableToFetchCarbon' that inherits from the built-in 'Exception' class
 class UnableToFetchCarbon(Exception):
-    """This class defines a custom exception named 'UnableToFetchCarbon' that inherits from the built-in 'Exception' class.
-    This exception can be raised when there is an issue with fetching carbon data."""
+    """
+    This custom exception class can be raised when there is an issue with fetching carbon data.
+    By defining a custom exception, it becomes easier to handle and identify specific errors related to fetching carbon data.
+    """
     pass
-
