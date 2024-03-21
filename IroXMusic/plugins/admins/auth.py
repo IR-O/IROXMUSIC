a2dfa55d291c68698231ffc0f6d5ef5cc6b89a57
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:46:05 +0530

    improve

--- IroXMusic/plugins/admins/auth.py
+++ IroXMusic/plugins/admins/auth.py
@@ -1,61 +1,67 @@
-from pyrogram import filters
-from pyrogram.types import Message
+import asyncio
+from typing import List, Dict
 
-from IroXMusic import app
-from IroXMusic.utils import extract_user, int_to_alpha
-from IroXMusic.utils.database import (
-    delete_authuser,
-    get_authuser,
+import pyrogram.filters
+from pyrogram.types import Message
+from pyrogram import Client as App
+
+from IroXMusic.utils import (
+    extract_user,
+    int_to_alpha,
     get_authuser_names,
+    get_authuser,
+    delete_authuser,
     save_authuser,
 )
-from IroXMusic.utils.decorators import AdminActual, language
+from IroXMusic.utils.decorators import AdminActual
 from IroXMusic.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
-@app.on_message(filters.command("auth") & filters.group & ~BANNED_USERS)
+@app.on_message(pyrogram.filters.command("auth") & pyrogram.filters.group & ~BANNED_USERS)
 @AdminActual
-async def auth(client, message: Message, _):
+async def auth_command(client: App, message: Message):
+    """Add a user to the authorized users list."""
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
     token = await int_to_alpha(user.id)
-    _check = await get_authuser_names(message.chat.id)
-    count = len(_check)
-    if int(count) == 25:
+    auth_users = await get_authuser_names(message.chat.id)
+    count = len(auth_users)
+    if count == 25:
         return await message.reply_text(_["auth_1"])
-    if token not in _check:
+    if token not in auth_users:
         assis = {
             "auth_user_id": user.id,
             "auth_name": user.first_name,
             "admin_id": message.from_user.id,
             "admin_name": message.from_user.first_name,
         }
-        get = adminlist.get(message.chat.id)
-        if get:
-            if user.id not in get:
-                get.append(user.id)
+        chat_admins = adminlist.get(message.chat.id)
+        if chat_admins:
+            if user.id not in chat_admins:
+                chat_admins.append(user.id)
         await save_authuser(message.chat.id, token, assis)
         return await message.reply_text(_["auth_2"].format(user.mention))
     else:
         return await message.reply_text(_["auth_3"].format(user.mention))
 
 
-@app.on_message(filters.command("unauth") & filters.group & ~BANNED_USERS)
+@app.on_message(pyrogram.filters.command("unauth") & pyrogram.filters.group & ~BANNED_USERS)
 @AdminActual
-async def unauthusers(client, message: Message, _):
+async def unauth_command(client: App, message: Message):
+    """Remove a user from the authorized users list."""
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
     token = await int_to_alpha(user.id)
     deleted = await delete_authuser(message.chat.id, token)
-    get = adminlist.get(message.chat.id)
-    if get:
-        if user.id in get:
-            get.remove(user.id)
+    chat_admins = adminlist.get(message.chat.id)
+    if chat_admins:
+        if user.id in chat_admins:
+            chat_admins.remove(user.id)
     if deleted:
         return await message.reply_text(_["auth_4"].format(user.mention))
     else:
@@ -63,27 +69,27 @@ async def unauthusers(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(["authlist", "authusers"]) & filters.group & ~BANNED_USERS
+    pyrogram.filters.command(["authlist", "authusers"]) & pyrogram.filters.group & ~BANNED_USERS
 )
-@language
-async def authusers(client, message: Message, _):
-    _wtf = await get_authuser_names(message.chat.id)
-    if not _wtf:
+async def authusers_command(client: App, message: Message):
+    """List the authorized users in the chat."""
+    auth_users = await get_authuser_names(message.chat.id)
+    if not auth_users:
         return await message.reply_text(_["setting_4"])
     else:
         j = 0
-        mystic = await message.reply_text(_["auth_6"])
+        reply_message = await message.reply_text(_["auth_6"])
         text = _["auth_7"].format(message.chat.title)
-        for umm in _wtf:
-            _umm = await get_authuser(message.chat.id, umm)
-            user_id = _umm["auth_user_id"]
-            admin_id = _umm["admin_id"]
-            admin_name = _umm["admin_name"]
+        async for auth_user in auth_users:
             try:
+                user_data = await get_authuser(message.chat.id, auth_user)
+                user_id = user_data["auth_user_id"]
+                admin_id = user_data["admin_id"]
+                admin_name = user_data["admin_name"]
                 user = (await app.get_users(user_id)).first_name
                 j += 1
-            except:
+            except Exception:
                 continue
             text += f"{j}➤ {user}[<code>{user_id}</code>]\n"
             text += f"   {_['auth_8']} {admin_name}[<code>{admin_id}</code>]\n\n"
-        await mystic.edit_text(text, reply_markup=close_markup(_))
+        await reply_message.edit_text(text, reply_markup=close_markup(_))
