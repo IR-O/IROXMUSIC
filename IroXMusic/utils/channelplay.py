7c60180cf7db6d593df330dcefd9a40bd3e1e5eb
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:50 +0530

    improve

--- IroXMusic/utils/channelplay.py
+++ IroXMusic/utils/channelplay.py
@@ -1,23 +1,30 @@
 from IroXMusic import app
 from IroXMusic.utils.database import get_cmode
 
+async def get_channel_playCB(_, command: str, CallbackQuery) -> tuple[str, str | None]:
+    """Get the chat ID and channel title for the given CallbackQuery.
 
-async def get_channeplayCB(_, command, CallbackQuery):
+    If the command is "c", get the chat ID from the database and the channel title from the chat.
+    If the command is not "c", get the chat ID from the CallbackQuery and set the channel title to None.
+
+    Returns:
+        A tuple of the chat ID and the channel title (or None if the channel title could not be retrieved).
+    """
+    chat_id = None
+    channel = None
+
     if command == "c":
         chat_id = await get_cmode(CallbackQuery.message.chat.id)
         if chat_id is None:
             try:
-                return await CallbackQuery.answer(_["setting_7"], show_alert=True)
+                if _["setting_7"] is not None:
+                    await CallbackQuery.answer(_["setting_7"], show_alert=True)
             except:
-                return
+                pass
+            return
+
         try:
-            channel = (await app.get_chat(chat_id)).title
-        except:
-            try:
-                return await CallbackQuery.answer(_["cplay_4"], show_alert=True)
-            except:
-                return
-    else:
-        chat_id = CallbackQuery.message.chat.id
-        channel = None
-    return chat_id, channel
+            chat = await app.get_chat(chat_id)
+            if chat is not None and chat.title is not None:
+                channel = chat.title
+        except
