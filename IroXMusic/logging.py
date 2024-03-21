c674a05ee7194eef20679a02047c097a29bdac82
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:46:56 +0530

    comment

--- IroXMusic/logging.py
+++ IroXMusic/logging.py
@@ -1,19 +1,19 @@
-import logging
+import logging  # Import the logging module
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
+    level=logging.INFO,  # Set the root logger level to INFO, so that all messages with level INFO and above will be tracked
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",  # Set the log format
+    datefmt="%d-%b-%y %H:%M:%S",  # Set the date format
     handlers=[
-        logging.FileHandler("log.txt"),
-        logging.StreamHandler(),
+        logging.FileHandler("log.txt"),  # Create a FileHandler to write logs to a file named 'log.txt'
+        logging.StreamHandler(),  # Create a StreamHandler to write logs to the console
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)  # Set the logger level for 'httpx' to ERROR, so that only ERROR level messages and above will be tracked
+logging.getLogger("pyrogram").setLevel(logging.ERROR)  # Set the logger level for 'pyrogram' to ERROR
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)  # Set the logger level for 'pytgcalls' to ERROR
 
 
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+def LOGGER(name: str) -> logging.Logger:  # Define a function to get a named logger
+    return logging.getLogger(name)  # Return the named logger
