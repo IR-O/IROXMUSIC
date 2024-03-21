99afa57cc444468ed776386cf5de48e70a4f7b80
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:04 +0530

    improve

--- IroXMusic/plugins/bot/settings.py
+++ IroXMusic/plugins/bot/settings.py
@@ -1,14 +1,5 @@
-# This function is called when the /settings or /setting command is issued in a group chat
+# This function handles the /settings and /setting command in a group chat
 # It shows the settings menu for the group chat
+def handle_settings_command(update, context):
+    # code to show settings menu
 
-
-# This function is called when the "settings_helper" button is pressed in the settings menu
-# It answers the callback query and edits the message to show the same settings menu
-
-
-# This function is called when the "settingsback_helper" button is pressed in the settings menu
-# It answers the callback query and edits the message to show the previous menu or the private panel of the bot owner
-
-
-# This function is called when a button press does not require admin rights
-# It handles different commands such as search answer, play mode answer, play type answer, auth answer, and vote mode answer
