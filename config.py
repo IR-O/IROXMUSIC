60eedefff5e2c658e6d1bdd34eb0e4e37dc2a927
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:07 +0530

    improve

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ import sys
 from typing import Optional
 
 import pyrogram
-from pyrogram.errors import ValueError
+from pyrogram.errors import Value Error
 from pydantic import BaseModel
 
+class CustomBaseModel(BaseModel):
+    def dict(self, *args, **kwargs):
+        return super().dict(exclude_unset=True, *args, **kwargs)
 
