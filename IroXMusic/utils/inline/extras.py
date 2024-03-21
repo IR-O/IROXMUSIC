71dc3e54c72f4c912c87cfe10ff6e1392d1cb5e0
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:34 +0530

    improve

--- IroXMusic/utils/inline/extras.py
+++ IroXMusic/utils/inline/extras.py
@@ -1,41 +1,21 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
 from config import SUPPORT_CHAT
 
+def create_button(_, text, url=None, callback_data=None):
+    if url:
+        return InlineKeyboardButton(text=_["S_B_9"], url=url)
+    elif callback_data:
+        return InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=callback_data)
+
+def create_markup(_, text, url=None, callback_data=None):
+    button = create_button(_, text, url, callback_data)
+    return InlineKeyboardMarkup([[button]])
 
 def botplaylist_markup(_):
-    buttons = [
-        [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-        ],
-    ]
-    return buttons
-
+    return create_markup(_, _["S_B_9"], SUPPORT_CHAT, "close")
 
 def close_markup(_):
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ]
-        ]
-    )
-    return upl
-
+    return create_markup(_, _["CLOSE_BUTTON"], callback_data="close")
 
 def supp_markup(_):
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_9"],
-                    url=SUPPORT_CHAT,
-                ),
-            ]
-        ]
-    )
-    return upl
+    return create_markup(_, _["S_B_9"], SUPPORT_CHAT)
