43a34ac9b3c3fb67332c0fba07195adb1e2d771a
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:35 +0530

    improve

--- IroXMusic/utils/inline/__init__.py
+++ IroXMusic/utils/inline/__init__.py
@@ -1,7 +1,11 @@
-from .extras import *
-from .help import *
-from .play import *
-from .queue import *
-from .settings import *
-from .speed import *
-from .start import *
+from . import extras, help, play, queue, settings, speed, start
+
+# Alternatively, if you prefer to use the original names for the modules, you can use the following import style:
+
+import .extras as extras
+import .help as help
+import .play as play
+import .queue as queue
+import .settings as settings
+import .speed as speed
+import .start as start
