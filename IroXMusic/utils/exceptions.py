04df3a24059965022fef5345c51efffb228ec205
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:36 +0530

    comment

--- IroXMusic/utils/exceptions.py
+++ IroXMusic/utils/exceptions.py
@@ -1,4 +1,4 @@
-# Define a custom exception class named 'AssistantErr' which inherits from the built-in 'Exception' class.
+# Define a custom exception class named 'AssistantErr' that inherits from the built-in 'Exception' class.
 # This class takes a single argument 'error' during initialization, which is a string message describing the error.
 # The **init** method calls the parent class's constructor and passes the formatted error message to it.
 class AssistantErr(Exception):
