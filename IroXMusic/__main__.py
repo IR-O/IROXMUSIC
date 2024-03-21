080346d5fa8122f8119ed97847cff9b471095cc3
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:09 +0530

    improve

--- IroXMusic/__main__.py
+++ IroXMusic/__main__.py
@@ -1,5 +1,21 @@
 import asyncio
 import importlib
 import sys
-import os
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os:path.abspath(os.path.join(current_directory, ".."))
+sys.path.insert(0, parent_directory)
+
+def import_module(module_name):
+    try:
+        return importlib.import_module(module_name)
+    except ModuleNotFoundError as e:
+        print(f"Error: {e}. Check the module name and try again.")
+        sys.exit(1)
+
+def run_main():
+    main_module = import_module("main")
+    main_module.main()
+
+if __name__ == "__main__":
+    run_main()
