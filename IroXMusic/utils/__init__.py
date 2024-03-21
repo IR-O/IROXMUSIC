8059147d3f08a2240107bc7b648ba4befa8caed3
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:28 +0530

    improve

--- IroXMusic/utils/__init__.py
+++ IroXMusic/utils/__init__.py
@@ -1,8 +1,8 @@
-from .channelplay import *
-from .database import *
-from .decorators import *
-from .extraction import *
-from .formatters import *
-from .inline import *
-from .pastebin import *
-from .sys import *
+import channelplay
+import database
+import decorators
+import extraction
+import formatters
+import inline
+import pastebin
+import sys
