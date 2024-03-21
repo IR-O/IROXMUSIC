14ed09134bb600125ab272eda31a76da79e660c1
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:46:59 +0530

    comment

--- IroXMusic/utils/inline/stats.py
+++ IroXMusic/utils/inline/stats.py
@@ -1,22 +1,31 @@
-from typing import List, Union
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from typing import List, Union  # Importing List and Union from typing module
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup  # Importing InlineKeyboardButton and InlineKeyboardMarkup from pyrogram.types module
 
 
 def button(text: str, callback_data: str) -> InlineKeyboardButton:
-    return InlineKeyboardButton(text, callback_data=callback_data)
+    # This function creates an InlineKeyboardButton object with the given text and callback_data
+    # Parameters:
+    #   text (str): The text to be displayed on the button
+    #   callback_data (str): The data to be sent back to the bot when the button is pressed
+    # Returns:
+    #   InlineKeyboardButton: A button object with the given text and callback_data
 
 
 def keyboard_markup(buttons: List[List[Union[InlineKeyboardButton, str]]]) -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(buttons)
+    # This function creates an InlineKeyboardMarkup object with the given list of buttons
+    # Parameters:
+    #   buttons (List[List[Union[InlineKeyboardButton, str]]]): A list of lists containing InlineKeyboardButton objects or strings (representing text for rows)
+    # Returns:
+    #   InlineKeyboardMarkup: A markup object with the given buttons
 
 
 def stats_buttons(_, status):
-    not_sudo = [button(_["SA_B_1"], "TopOverall")] if _["SA_B_1"] else []
-    sudo = [button(_["SA_B_2"], "bot_stats_sudo"), button(_["SA_B_3"], "TopOverall")] if _["SA_B_2"] and _["SA_B_3"] else []
-    buttons = [sudo if status and sudo else not_sudo, [button(_["CLOSE_BUTTON"], "close")]]
-    return keyboard_markup(buttons)
+    # This function creates a list of buttons for the stats command
+    # Parameters:
+    #   _ (dict): A dictionary containing bot settings
+    #   status (bool): A boolean indicating whether the user is a sudo user or not
+    # Returns:
+    #   InlineKeyboardMarkup: A markup object containing the buttons
 
-
-def back_stats_buttons(_):
-    buttons = [[button(_["BACK_BUTTON"], "stats_back"), button(_["CLOSE_BUTTON"], "close")]]
-    return keyboard_markup(buttons)
+    not_sudo = [button(_["SA_B_1"], "TopOverall")] if _["SA_B_1"] else []  # If SA_B_1 is present in the dictionary, create a button with its value and "TopOverall" as callback_data
+    sudo = [button(_["SA_B_2"], "bot_stats_sudo"), button(_["SA_B_3"], "TopOverall")] if _["SA_B_2"] and _["SA_B_3"] else []  # If both
