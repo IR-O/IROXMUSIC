aa3991ea3e9eaf86b296cdb24e24c900973c57b1
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:45:37 +0530

    improve

--- IroXMusic/plugins/sudo/logger.py
+++ IroXMusic/plugins/sudo/logger.py
@@ -1,4 +1,6 @@
-from pyrogram import filters
+from pyrogram import filters, StopPropagation
+
+from pyrogram.errors import FloodWait
 
 from IroXMusic import app
 from IroXMusic.misc import SUDOERS, LOVE
@@ -6,18 +8,33 @@ from IroXMusic.utils.database import add_off, add_on
 from IroXMusic.utils.decorators.language import language
 
 
-@app.on_message(filters.command(["logger"]) & SUDOERS & LOVE)
+@app.on_message(filters.private & filters.command(["logger"]) & SUDOERS & LOVE)
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
-        await add_on(2)
-        await message.reply_text(_["log_2"])
-    elif state == "disable":
-        await add_off(2)
-        await message.reply_text(_["log_3"])
-    else:
-        await message.reply_text(usage)
+    if state not in ("enable", "disable"):
+        return await message.reply_text(usage)
+    chat_id = message.chat.id
+    try:
+        if state == "enable":
+            await add_on(chat_id)
+            await message.reply_text(_["log_2"])
+        elif state == "disable":
+            await add_off(chat_id)
+            await message.reply_text(_["log_3"])
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+        raise StopPropagation
+
+
+@app.on_message(filters.group & filters.command(["logger"]) & SUDOERS & LOVE)
+@language
+async def logger_group(client, message, _):
+    if not message.from_user.id in SUDOERS:
+        return
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await
