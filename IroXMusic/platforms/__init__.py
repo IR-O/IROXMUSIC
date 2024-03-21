bc4ad2030b7babd65690d5502b91b6f0b68f6674
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:46:54 +0530

    comment

--- IroXMusic/platforms/__init__.py
+++ IroXMusic/platforms/__init__.py
@@ -1,7 +1,11 @@
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
+# Import various API classes for different music streaming platforms and other services
+from .Apple import AppleAPI  # Apple Music API
+from .Carbon import CarbonAPI  # TikTok (formerly Musical.ly) API
+from .Resso import RessoAPI  # Resso API
+from .Soundcloud import SoundAPI  # Soundcloud API
+from .Spotify import SpotifyAPI  # Spotify API
+from .Telegram import TeleAPI  # Telegram API
+from .Youtube import YouTubeAPI  # YouTube API
+
+# Each API class provides a set of methods to interact with the corresponding service's API
+# These classes can be used to build custom integrations with these services in your application
