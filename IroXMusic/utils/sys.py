503f102031e74589e6a1f8b28f41c1d1320c16e7
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:04 +0530

    improve

--- IroXMusic/utils/sys.py
+++ IroXMusic/utils/sys.py
@@ -1,16 +1,17 @@
 import time
 import psutil
 from typing import Tuple
+from datetime import timedelta
 
-from IroXMusic.misc import _boot_
-from IroXMusic.utils.formatters import get_readable_time
+def format_time(seconds: int) -> str:
+    return str(timedelta(seconds=seconds))
 
 async def bot_sys_stats() -> Tuple[str, str, str, str]:
     """
     This function returns the bot's uptime and system statistics (CPU, RAM, and DISK usage) as strings.
     """
     bot_uptime = int(time.time() - _boot_)  # Calculate bot uptime in seconds
-    UP = f"{get_readable_time(bot_uptime)}"  # Convert uptime to a human-readable format
+    UP = format_time(bot_uptime)  # Convert uptime to a human-readable format
 
     try:
         # Set the minimum interval to 0.1 seconds to avoid potential freezing issues
