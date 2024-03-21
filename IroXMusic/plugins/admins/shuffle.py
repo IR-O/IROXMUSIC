d4b383f0b92851789793aa43f64facc6c6570504
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:47:00 +0530

    comment

--- IroXMusic/plugins/admins/shuffle.py
+++ IroXMusic/plugins/admins/shuffle.py
@@ -3,31 +3,41 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from IroXMusic import app
-from IroXMusic.misc import db
-from IroXMusic.utils.decorators import AdminRightsCheck
-from IroXMusic.utils.inline import close_markup
-from config import BANNED_USERS
-
+import db  # Database module
+from . import app, AdminRightsCheck, close_markup, _  # IroXMusic module and utility functions
+from config import BANNED_USERS  # Banned users list
 
 @app.on_message(
-    filters.command(["shuffle", "cshuffle"]) & filters.group & ~BANNED_USERS
+    filters.command(["shuffle", "cshuffle"]) &  # Checks for "shuffle" or "cshuffle" command
+    filters.group &  # Only process messages in groups
+    ~BANNED_USERS  # Exclude banned users
 )
-@AdminRightsCheck
+@AdminRightsCheck  # Checks for admin privileges
 async def admins(Client, message: Message, _, chat_id):
+    # Get the current chat's database entry
     check = db.get(chat_id)
     if not check:
-        return await message.reply_text(_["queue_2"])
+        return await message.reply_text(_["queue_2"])  # If no entry, return a message
+
     try:
+        # Remove the first item from the list
         popped = check.pop(0)
     except:
-        return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))
+        return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))  # If an error occurs, return a message
+
+    # Get the updated chat's database entry
     check = db.get(chat_id)
     if not check:
-        check.insert(0, popped)
-        return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))
+        check.insert(0, popped)  # If no entry, insert the removed item back
+        return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))  # Return a message
+
+    # Shuffle the list and insert the removed item at the beginning
     random.shuffle(check)
     check.insert(0, popped)
+
+    # Reply with a success message
     await message.reply_text(
-        _["admin_16"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_16"].format(message.from_user.mention),  # User mention
+        reply_markup=close_markup(_)  # Close the inline button
     )
+
