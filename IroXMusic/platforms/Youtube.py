844b13a9057241fc25ed476878f6915a38c8cea1
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:48:06 +0530

    improve

--- IroXMusic/platforms/Youtube.py
+++ IroXMusic/platforms/Youtube.py
@@ -1,40 +1,16 @@
 import asyncio
-import os
-import re
 from typing import AsyncContextManager, List, Optional, Union
 
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch  # A module for YouTube search
-from pathlib import Path  # A library to handle file paths
-
-import IroXMusic.utils.database as database  # A module for database-related operations
-from IroXMusic.utils.formatters import time_to_seconds  # A function to convert time to seconds
-
-# This is an asynchronous context manager, which is used to manage resources in an asynchronous context.
-AsyncContextManager
-
-# List, Optional, and Union are types from the typing module, which provides optional static typing for Python.
-List, Optional, Union
-
-# yt_dlp is a fork of youtube-dl with additional features and fixes.
-import yt_dlp
-
-# Pyrogram's MessageEntityType is an enumeration of the different types of message entities.
-MessageEntityType
-
-# Pyrogram's Message is a class representing a message in a chat.
-Message
-
-# VideosSearch is a class from youtubesearchpython that provides a simple way to search for YouTube videos.
-VideosSearch
-
-# Path is a class from pathlib that provides an object-oriented interface to filesystem paths.
-Path
-
-# The database module contains functions for interacting with the application's database.
-IroXMusic.utils.database
-
-# The formatters module contains a function for converting time formats to seconds.
-IroXMusic.utils.formatters
+from youtubesearchpython import VideosSearch
+from pathlib import Path
+from IroXMusic.utils.database import (
+    add_new_user,
+    get_user,
+    get_user_from_event,
+    is_active_chat,
+    is_premium,
+)
+from IroXMusic.utils.formatters import time_to_seconds
