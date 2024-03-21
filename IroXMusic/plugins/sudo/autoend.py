ab7f740edd03c90b3883443600b8856ad17969f6
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:38 +0530

    comment

--- IroXMusic/plugins/sudo/autoend.py
+++ IroXMusic/plugins/sudo/autoend.py
@@ -1,23 +1,19 @@
-from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram import filters # Importing filters module from pyrogram library
+from pyrogram.types import Message # Importing Message class from pyrogram.types module
 
-from IroXMusic import app
-from IroXMusic.misc import SUDOERS, LOVE
-from IroXMusic.utils.database import autoend_on, autoend_off
+from IroXMusic import app # Importing app instance from IroXMusic module
+from IroXMusic.misc import SUDOERS, LOVE # Importing SUDOERS and LOVE from IroXMusic.misc module
+from IroXMusic.utils.database import autoend_on, autoend_off # Importing autoend_on and autoend_off functions from IroXMusic.utils.database module
 
-@app.on_message(filters.command("autoend") & SUDOERS & LOVE)
-async def auto_end_stream(_, message: Message):
-    USAGE = "ᴇxᴀᴍᴘʟᴇ :\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+@app.on_message(filters.command("autoend") & SUDOERS & LOVE) # Decorator to listen for /autoend command from sudoers and LOVE users
+async def auto_end_stream(_, message: Message): # Defining the asynchronous function auto_end_stream
+    USAGE = "ᴇxᴀᴍᴘʟᴇ :\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]" # Defining the usage message
+    # Checking if the length of the command is not equal to 2
     if len(message.command) != 2:
-        return await message.reply_text(USAGE)
-    state = message.text.split(None, 1)[1].strip().lower()
+        return await message.reply_text(USAGE) # Replying to the user with the usage message
+    state = message.text.split(None, 1)[1].strip().lower() # Extracting the state (enable or disable) from the user input
+    # Checking if the state is equal to "enable"
     if state == "enable":
-        await autoend_on()
+        await autoend_on() # Calling the autoend_on function
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
-        )
-    elif state == "disable":
-        await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
-    else:
-        await message.reply_text(USAGE)
+            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғ
