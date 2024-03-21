ac842f3400894e90a8a8bc8c74aedee8e8b2ec62
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:51:41 +0530

    comment

--- IroXMusic/platforms/Soundcloud.py
+++ IroXMusic/platforms/Soundcloud.py
@@ -1,39 +1,43 @@
-from os import path
+from os import path  # Importing path from os module for file path manipulation
 
-from yt_dlp import YoutubeDL
+from yt_dlp import YoutubeDL  # Importing YoutubeDL from yt\_dlp module for downloading youtube videos
 
-from IroXMusic.utils.formatters import seconds_to_min
-
+from IroXMusic.utils.formatters import seconds_to_min  # Importing seconds\_to\_min from formatters for converting seconds to minutes
 
 class SoundAPI:
     def __init__(self):
+        """
+        Initializes the SoundAPI class with default options for YoutubeDL.
+        """
         self.opts = {
-            "outtmpl": "downloads/%(id)s.%(ext)s",
-            "format": "best",
-            "retries": 3,
-            "nooverwrites": False,
-            "continuedl": True,
+            "outtmpl": "downloads/%(id)s.%(ext)s",  # Output template for the downloaded file
+            "format": "best",  # Format of the downloaded file
+            "retries": 3,  # Number of retries for failed downloads
+            "nooverwrites": False,  # Whether to overwrite existing files
+            "continuedl": True,  # Whether to continue downloading a partially downloaded file
         }
 
     async def valid(self, link: str):
+        """
+        Checks if the given link is a soundcloud link.
+        :param link: The link to check
+        :return: True if the link is a soundcloud link, False otherwise
+        """
         if "soundcloud" in link:
             return True
         else:
             return False
 
     async def download(self, url):
-        d = YoutubeDL(self.opts)
+        """
+        Downloads the given url and returns the track details and the filepath of the downloaded file.
+        :param url: The url to download
+        :return: A tuple containing the track details and the filepath of the downloaded file
+        """
+        d = YoutubeDL(self.opts)  # Initialize YoutubeDL with the default options
         try:
-            info = d.extract_info(url)
+            info = d.extract_info(url)  # Extract information about the url
         except:
-            return False
-        xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
-        duration_min = seconds_to_min(info["duration"])
-        track_details = {
-            "title": info["title"],
-            "duration_sec": info["duration"],
-            "duration_min": duration_min,
-            "uploader": info["uploader"],
-            "filepath": xyz,
-        }
-        return track_details, xyz
+            return False  # If an exception occurs, return False
+        xyz = path.join("downloads", f"{info['id']}.{info['ext']}")  # Construct the filepath of the downloaded file
+        duration
