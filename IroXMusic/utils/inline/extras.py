d8676887320f36822fe7173569aa42484948be92
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-03-22 00:53:06 +0530

    improve

--- IroXMusic/utils/inline/extras.py
+++ IroXMusic/utils/inline/extras.py
@@ -7,38 +7,39 @@ def create_button(text, url=None, callback_data=None):
     """
     Create an InlineKeyboardButton object with the given text, URL, and callback data.
     """
-    button = InlineKeyboardButton(text, url=url, callback_data=callback_data)
+    if callback_data is not None:
+        button = InlineKeyboardButton(text, callback_data=callback_data)
+    elif url is not None:
+        button = InlineKeyboardButton(text, url=url)
+    else:
+        raise ValueError("Either url or callback_data must be provided")
     return button
 
-def create_markup(button):
+def create_markup(buttons):
     """
-    Create an InlineKeyboardMarkup object with the given button.
+    Create an InlineKeyboardMarkup object with the given list of buttons.
     """
-    markup = InlineKeyboardMarkup([[button]])
+    markup = InlineKeyboardMarkup(buttons)
     return markup
+
+def get_support_button(text):
+    """
+    Create a support button with the given text and URL.
+    """
+    url = SUPPORT_CHAT
+    button = create_button(text, url)
+    return button
 
 def botplaylist_markup():
     """
     Create a markup for botplaylist with the given text, URL, and callback data.
     """
-    url = SUPPORT_CHAT
-    button = create_button(_["S_B_9"], url, "close")
-    markup = create_markup(button)
+    button = create_button(_["S_B_9"], url=SUPPORT_CHAT, callback_data="close")
+    markup = create_markup([[button]])
     return markup
 
 def close_markup():
     """
     Create a markup for closing the current window with the given text and callback data.
     """
-    button = create_button(_["CLOSE_BUTTON"], callback_data="close")
-    markup = create_markup(button)
-    return markup
-
-def supp_markup():
-    """
-    Create a markup for support with the given text and URL.
-    """
-    url = SUPPORT_CHAT
-    button = create_button(_["S_B_9"], url)
-    markup = create_markup(button)
-    return markup
+    button
